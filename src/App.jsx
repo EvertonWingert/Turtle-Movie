75c9a26163239261a2a4d0713876f2e9ad11f10d
Evander Inacio <evander.20112@hotmail.com>
2022-09-26 22:58:59 -0300

    App :fire:

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Header } from "./components/Header"
-import { Detail } from './pages/Detail/Detail'
-import { Persons } from './pages/Persons/Persons'
-import { Home } from './pages/Home/Home'
-import { Movies } from './pages/Movies/Movies'
-import { Serie } from './pages/Series/Series'
-import { Person } from './pages/Persons/Person'
-import GlobalStyle from './styles/global'
-import { Search } from "./pages/search/search"
+import { Home } from "./pages/Home/Home"
+import { Movies } from "./pages/Movies/Movies"
+import { Serie } from "./pages/Series/Series"
+import { Detail } from "./pages/Detail/Detail"
+import { Persons } from "./pages/Persons/Persons"
+import { Person } from "./pages/Persons/Person"
+import { Search } from "./pages/Search/Search"
 import { Random } from "./pages/Random/Random"
+import GlobalStyle from "./styles/global"
 
 
 function App() {
